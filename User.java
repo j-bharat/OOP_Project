ad761859ffac1aab69573d319ba4db7eae3ff874
Bharat Jain <77245003+j-bharat@users.noreply.github.com>
2022-12-03 19:39:43 +0530

    Updated User.java

--- User.java
+++ User.java
@@ -1,19 +1,46 @@
-public class User
+import .Plans;
+
+
+public class User 
 {
     String name;
     String ID;
-    String passkey;
-    String bhawan;
-    Plans plan;
     long ph_no;
-    int washes_remaining;
+    //String password;
+    Bhawan bhawan;
+    Plans plan;
+    
+    
+    int washes_count;
     float total_expense;
     
+    public User (String name, String ID, long ph_no, Bhawan bhawan, Plans plan) {
+//    	super(bhawan_name);
+    
+    	this.ID = ID;
+		this.name = name;
+		this.ph_no = ph_no;
+		this.plan = plan;
+		this.bhawan = bhawan;
+		
+		this.washes_count = 0;
+	    float total_expense = 0;
+	    
+	    
+    }
+    
+    public Plans getPlan() {
+		return plan;
+	}
 
-
-    String status(User obj, Plan cost) {
+	public void setPlan(Plans plan) {
+		this.plan = plan;
+	}
+    
+	
+    String status(Plans cost) {
         
-        return "Total expense " + (this.plan.no_of_washes-this.washes_remaining)*this.plan.cost + " Washes remaining " +  this.washes_remaining; 
+        return "Total expense " + plan.get_cost() + " Washes remaining " +  this.washes_count; 
         
     }
 
@@ -28,13 +55,13 @@ public class User
 	}
 
     public String getBhawan() {
-		return bhawan ;
+		
+    	return bhawan.name;
 	} 
 	
-
-	public void setHostel(String bhawan) {
-		this.bhawan = bhawan;
-	}
+	
+	
+	
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -42,21 +69,22 @@ public class User
 	public void ID(String ID) {
 		this.ID = ID;
 	}
-	public void selectPlan(int index) {
-		this.plan = Plans.getPlans().get(index);
-		washes_remaining -= plan.no_of_washes;
-	}
 	
-	public String clothesGiven(double weight) {
-		laundryList.add(new wash(weight));
-		if(washes_remaining == 0) {
-			return "No washes remaining, please recharge";
-		}
-		else {
-			washes_remaining -= 1;
-            return "Wash Accepted";
-		}
-		
+//	public void selectPlan(int index) {
+//		this.plan = Plans.getPlans().get(index);
+//		washes_remaining -= plan.get_no_of_washes();
+//	}
+//	
+//	public String clothesGiven(double weight) {
+//		laundryList.add(new wash(weight));
+//		if(washes_remaining == 0) {
+//			return "No washes remaining, please recharge";
+//		}
+//		else {
+//			washes_remaining -= 1;
+//            return "Wash Accepted";
+//		}
+//		
 		
 	}
 }
